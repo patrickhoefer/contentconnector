fe4a5400e7b4994ff825fd572231c3be9a8976ad
floriankofler <fkofler@gmx.at>
2016-08-25 16:55:34 +0200

    read stopwords

--- contentconnector-lucene/src/main/java/com/gentics/cr/lucene/analysis/WkoAnalyzer.java
+++ contentconnector-lucene/src/main/java/com/gentics/cr/lucene/analysis/WkoAnalyzer.java
@@ -1,8 +1,12 @@
 package com.gentics.cr.lucene.analysis;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
-
+import java.io.StringWriter;
+import java.util.Properties;
+import java.io.FileReader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.TokenFilter;
@@ -10,21 +14,46 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import com.gentics.cr.configuration.GenericConfiguration;
-
+import java.io.BufferedReader;
 import com.gentics.cr.lucene.LuceneVersion;
+import org.apache.lucene.util.Version;
+import com.gentics.cr.lucene.indexer.IndexerUtil;
+
 import org.apache.log4j.Logger;
 
-public class WkoAnalyzer extends Analyzer {
+public class WkoAnalyzer extends StopwordAnalyzerBase {
 	
+	private static final String STOP_WORD_FILE_KEY = "STOPWORDFILE";
 	private static final Logger LOGGER = Logger.getLogger(WkoAnalyzer.class);
 	
-	public WkoAnalyzer(GenericConfiguration conf){
-		super(conf);
+	public WkoAnalyzer(GenericConfiguration conf) throws IOException{
+		super(LuceneVersion.getVersion(),loadStopwordSet(new BufferedReader(new FileReader(IndexerUtil.getFileFromPath((String) conf.get(STOP_WORD_FILE_KEY)))), LuceneVersion.getVersion()));
+		LOGGER.error("Charrarrayset: "+stopwords);
+		LOGGER.error("Stopwordsfile: "+((String) conf.get(STOP_WORD_FILE_KEY)));
+		/*LOGGER.error("INTERVAL: "+((String) conf.get("interval")));
+		Properties p = conf.getProperties();
+		StringWriter sw = new StringWriter();
+		p.list(new PrintWriter(sw));
+		LOGGER.error("HMMM" + sw.toString());*/
 	}
+	public WkoAnalyzer(Version matchVersion, CharArraySet stopWords) {
+		    super(matchVersion, stopWords);
+	}
+	 
+	/*public WkoAnalyzer(LuceneVersion version){
+		//super(conf);
+		// File stopWordFile = IndexerUtil.getFileFromPath((String) config.get(STOP_WORD_FILE_KEY));
+		super(version,stopwords);
+		LOGGER.info()
+	}*/
 	public WkoAnalyzer(){
-		super();
+		super(LuceneVersion.getVersion());
 	}
 	
 	@Override
@@ -33,6 +62,7 @@ public class WkoAnalyzer extends Analyzer {
 		TokenStream ts = new StandardFilter(LuceneVersion.getVersion(), source);
 		ts = new LowerCaseFilter(LuceneVersion.getVersion(), ts);
 		ts = new GermanNormalizationFilterAdapted(ts);
+		ts = new StopFilter(LuceneVersion.getVersion(), ts, stopwords);
 		return new TokenStreamComponents(source,ts);
 	}
 
